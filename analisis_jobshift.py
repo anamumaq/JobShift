06468e2583c6080dbc06fe4872a000a494dfe9b5
anamumaq <ana.munozmaquera@gmail.com>
2022-05-29 15:26:32 -0500

    se elimino archivo .py y se cambio el nombre del notebook

--- analisis_jobshift.py
+++
@@ -1,8 +0,0 @@
-# Analisis de Job Shift
-# En base a dataset de Kagle
-###################################
-# Importar Librerías
-import pandas as pd
-# 1. Extraer Data
-
-# 2. Conociendo la forma de la data
