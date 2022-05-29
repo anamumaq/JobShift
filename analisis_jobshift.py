56d4fce559d7847f3bca0ac1925103ab406f9c7a
anamumaq <ana.munozmaquera@gmail.com>
2022-05-29 15:20:35 -0500

    Entrendimiento del problema estraccion de data, forma y datos faltantes parte1

--- analisis_jobshift.py
+++ analisis_jobshift.py
@@ -1,6 +1,8 @@
 # Analisis de Job Shift
 # En base a dataset de Kagle
-
+###################################
+# Importar Librerías
+import pandas as pd
 # 1. Extraer Data
+
 # 2. Conociendo la forma de la data
-# 
